4153f55d610fcecbcf0e4e4da1aa41e556a1e5fb
Everest Paudel <paudeleverest09@gmail.com>
2025-08-10 01:36:06 +0545

    Express Server setup

--- server.ts
+++ server.ts
@@ -1,1 +1,16 @@
+
+import app from './src/app'
+
+
+
+const startServer = ()=>{
+    const port = process.env.PORT ||3000
+    
+    app.listen(port ,()=>{
+        console.log(`The server is listeing http://localhost:${port}`)
+    })
+}
+
+startServer()
+
 console.log("Welcome to ebook api")
--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.get('/' ,(req ,res,next)=>{
+res.json({message :"Welcome to the ebook api"})
+})
+
 export default app
